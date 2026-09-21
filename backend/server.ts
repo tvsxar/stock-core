5d6f4274ebf59905c40ffe362df4d59a055c61f6
tvsxar <tarasofficial82@gmail.com>
2026-09-21 15:42:03 +0300

    feat: set up Express server

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,16 @@
 import "dotenv/config";
-import { pool } from './database/db.js';
+import express from "express";
 
-try {
-    const res = await pool.query('SELECT NOW()');
+const PORT = process.env.PORT || 1011;
 
-    console.log(res.rows);
-} catch {
-    console.error('Database query error')
-}
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
